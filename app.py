784b0021e9f962b612f8371c02f708b0f689a164
Vinav Chattree <vinav.chattree@vin.net>
2026-05-10 00:01:21 +0530

    New math features

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ def subtract(a,b):
 a=input("Enter first number")
 b=input("Enter second number")
 print(add(int(a),int(b)))
+print(subtract(int(a),int(b)))
+print("Multiplication coming soon")
+print("Divison coming soon")
